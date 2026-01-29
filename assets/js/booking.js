bfa1e03d89146a7850d5a1a364c10fe3eb82e80e
khairulazri-sys <khairulazri.jasferi@gmail.com>
2026-01-30 02:05:29 +0800

    update google apps script for booking

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -4,7 +4,7 @@
 */
 
 // --- 1. CONFIGURATION ---
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzhOjPc24_NAtPTgWowbMwTiCWKwUu3FnFf3Dk_ssyqFFFkKDGMA8VIWkn3sRHMA8/exec"; 
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoSVauXL3GmKgzgobtLCNbcxmb0Cj8mZdko32ChySLFROvICWKTQcrf3eHVEsVI3Bj/exec"; 
 const TIME_SLOTS = ["09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30"];
 
 const SEASON_START = "2026-03-05"; 
